2810ff4dbdd02b3b305a854fde3f889a10d53327
Jazcaroni <jazmin.ramirez@mayoral.com>
2023-10-10 20:02:25 +0200

    prueba unitaria motor

--- src/motor.spec.ts
+++ src/motor.spec.ts
@@ -1,16 +1,16 @@
 import { vi } from "vitest";
 import { sumarPuntuacion } from "./motor";
-import { partida } from "./modelo";
 
 describe("sumarPuntuacion", () => {
   it("Debería sumar correctamente el valorPunto de la carta y devolver mensaje de puntosTotales", () => {
     //Arrange
     const puntos: number = 7;
+    const partida = { puntosTotales: 0 };
     vi.spyOn(partida, "puntosTotales", "get").mockReturnValue(5);
     const resultadoEsperado: number = 12;
     //Act
-    sumarPuntuacion(puntos);
+    const resultado = sumarPuntuacion(puntos);
     //Assert
-    expect(resultadoEsperado).toBe(12);
+    expect(resultado).toBe(resultadoEsperado);
   });
 });
