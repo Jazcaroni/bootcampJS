0831752014656d9f8ca2ae1d393df5c4ca858425
Jazcaroni <jazmin.ramirez@mayoral.com>
2023-10-19 15:09:17 +0200

    modulo pruebas unitarias

--- src/motor.spec.ts
+++ src/motor.spec.ts
@@ -1,5 +1,9 @@
 import { vi } from "vitest";
-import { sumarPuntuacion } from "./motor";
+import {
+  sumarPuntuacion,
+  obtenerEstadoPartida,
+  generarNumeroDeCarta,
+} from "./motor";
 import { partida } from "./modelo";
 
 describe("sumarPuntuacion", () => {
@@ -12,5 +16,67 @@ describe("sumarPuntuacion", () => {
     const resultado = sumarPuntuacion(puntos);
     //Assert
     expect(resultado).toBe(resultadoEsperado);
+  });
+});
+
+describe("obtenerEstadoPartida", () => {
+  it("Debería de devolver PERDER_PARTIDA cuando se pasa del límite de los puntos", () => {
+    // Arrange
+    const resultadoEsperado = "PERDER_PARTIDA";
+    vi.spyOn(partida, "puntosTotales", "get").mockReturnValue(9);
+
+    // Act
+    const resultado = obtenerEstadoPartida();
+
+    // Assert
+    expect(resultado).toBe(resultadoEsperado);
+  });
+
+  it("Debería de devolver GANAR_PARTIDA cuando se llega al límite de los puntos", () => {
+    // Arrange
+    const resultadoEsperado = "GANAR_PARTIDA";
+    vi.spyOn(partida, "puntosTotales", "get").mockReturnValue(7.5);
+
+    // Act
+    const resultado = obtenerEstadoPartida();
+
+    // Assert
+    expect(resultado).toBe(resultadoEsperado);
+  });
+
+  it("Debería de devolver EN_CURSO cuando la puntuación está en el rango permitido", () => {
+    // Arrange
+    const resultadoEsperado = "EN_CURSO";
+    vi.spyOn(partida, "puntosTotales", "get").mockReturnValue(6);
+
+    // Act
+    const resultado = obtenerEstadoPartida();
+
+    // Assert
+    expect(resultado).toBe(resultadoEsperado);
+  });
+});
+
+describe("generarNumeroDeCarta", () => {
+  it("Debería sumar 2 al resultado si el número aleatorio es mayor que 7", () => {
+    // Arrange
+    vi.spyOn(partida, "numeroAleatorio", "get").mockReturnValue(8);
+
+    // Act
+    const resultado = generarNumeroDeCarta(partida.numeroAleatorio);
+
+    // Assert
+    expect(resultado).toBe(10);
+  });
+
+  it("No debería sumar 2 al resultado si el número aleatorio es menor o igual a 7", () => {
+    // Arrange
+    vi.spyOn(partida, "numeroAleatorio", "get").mockReturnValue(5);
+
+    // Act
+    const resultado = generarNumeroDeCarta(partida.numeroAleatorio);
+
+    // Assert
+    expect(resultado).toBe(5);
   });
 });
