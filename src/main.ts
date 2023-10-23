228f1937171f7ca727e56e4c1e15db651e632578
Jazcaroni <jazmin.ramirez@mayoral.com>
2023-10-23 14:11:02 +0200

    Laboratorio bucles 8.1

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,199 @@
-import { pedirCarta, mePlanto, nuevaPartida, simularPartida } from "./ui";
+//Laboratorio Bucles
+type Especialidad = "Medico de familia" | "Pediatra" | "Cardiólogo";
 
-const btnPedirCarta = document.getElementById("btnDameCarta");
-if (
-  btnPedirCarta !== undefined &&
-  btnPedirCarta !== null &&
-  btnPedirCarta instanceof HTMLButtonElement
-) {
-  btnPedirCarta.addEventListener("click", pedirCarta);
+interface Pacientes {
+  id: number;
+  nombre: string;
+  apellidos: string;
+  sexo: string;
+  temperatura: number;
+  frecuenciaCardiaca: number;
+  especialidad: Especialidad;
+  edad: number;
 }
 
-const btnMePlanto = document.getElementById("btn-mePlanto");
-if (
-  btnMePlanto !== undefined &&
-  btnMePlanto !== null &&
-  btnMePlanto instanceof HTMLButtonElement
-) {
-  btnMePlanto.addEventListener("click", mePlanto);
+const pacientes: Pacientes[] = [
+  {
+    id: 1,
+    nombre: "John",
+    apellidos: "Doe",
+    sexo: "Male",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 80,
+    especialidad: "Medico de familia",
+    edad: 44,
+  },
+  {
+    id: 2,
+    nombre: "Jane",
+    apellidos: "Doe",
+    sexo: "Female",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 70,
+    especialidad: "Medico de familia",
+    edad: 43,
+  },
+  {
+    id: 3,
+    nombre: "Junior",
+    apellidos: "Doe",
+    sexo: "Male",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 90,
+    especialidad: "Pediatra",
+    edad: 8,
+  },
+  {
+    id: 4,
+    nombre: "Mary",
+    apellidos: "Wien",
+    sexo: "Female",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 120,
+    especialidad: "Medico de familia",
+    edad: 20,
+  },
+  {
+    id: 5,
+    nombre: "Scarlett",
+    apellidos: "Somez",
+    sexo: "Female",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 110,
+    especialidad: "Cardiólogo",
+    edad: 30,
+  },
+  {
+    id: 6,
+    nombre: "Brian",
+    apellidos: "Kid",
+    sexo: "Male",
+    temperatura: 39.8,
+    frecuenciaCardiaca: 80,
+    especialidad: "Pediatra",
+    edad: 11,
+  },
+];
+// Apartado Obligatorio
+//a) Queremos extraer la lista de paciente que están asignados a la especialidad de Pediatría
+const obtenPacientesAsignadosAPediatria = (
+  pacientes: Pacientes[]
+): Pacientes[] => {
+  const pacientesPediatra: Pacientes[] = [];
+
+  for (let i = 0; i < pacientes.length; i++) {
+    if (pacientes[i].especialidad === "Pediatra") {
+      pacientesPediatra.push({ ...pacientes[i] });
+    }
+  }
+
+  return pacientesPediatra;
+};
+
+const pacientesPediatra = obtenPacientesAsignadosAPediatria(pacientes);
+console.log(pacientesPediatra);
+
+//b) Queremos extraer la lista de pacientes asignados a Pediatría y que tengan una edad menor de 10 años.
+const obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios = (
+  pacientes: Pacientes[]
+): Pacientes[] => {
+  const pacientesPediatriaMenoresDiez: Pacientes[] = [];
+
+  for (let i = 0; i < pacientes.length; i++) {
+    if (pacientes[i].especialidad === "Pediatra" && pacientes[i].edad < 10) {
+      pacientesPediatriaMenoresDiez.push({ ...pacientes[i] });
+    }
+  }
+  return pacientesPediatriaMenoresDiez;
+};
+const pacientesPediatriaMenoresDiez =
+  obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios(pacientes);
+console.log(pacientesPediatriaMenoresDiez);
+
+//Queremos activar el protocolo de urgencia si cualquier de los pacientes tiene un ritmo cardíaco superior a 100 pulsaciones por minuto y una temperatura corporal superior a 39 grados.
+
+//Es decir, crear una función que devuelve true/false dependiendo si se da la condición, algo así como:
+
+const activarProtocoloUrgencia = (pacientes: Pacientes[]): boolean => {
+  let activarProctolo = false;
+  for (let i = 0; i < pacientes.length; i++) {
+    if (
+      pacientes[i].temperatura > 39 &&
+      pacientes[i].frecuenciaCardiaca > 100
+    ) {
+      activarProctolo = true;
+      break;
+    }
+  }
+  return activarProctolo;
+};
+const seActivaComprobar = activarProtocoloUrgencia(pacientes);
+console.log(seActivaComprobar);
+
+//El pediatra no puede atender hoy a los pacientes, queremos reasignar los pacientes asignados a la especialidad de pediatría a la de medico de familia.
+
+const reasignaPacientesAMedicoFamilia = (
+  pacientes: Pacientes[]
+): Pacientes[] => {
+  const nuevosPacientes = [...pacientes];
+
+  for (let i = 0; i < nuevosPacientes.length; i++) {
+    if (nuevosPacientes[i].especialidad === "Pediatra") {
+      nuevosPacientes[i] = {
+        ...nuevosPacientes[i],
+        especialidad: "Medico de familia",
+      };
+    }
+  }
+
+  return nuevosPacientes;
+};
+
+const pacientesReasignados = reasignaPacientesAMedicoFamilia(pacientes);
+console.log(pacientesReasignados);
+
+//Queremos saber si podemos mandar al Pediatra a casa (si no tiene pacientes asignados), comprobar si en la lista hay algún paciente asignado a pediatría
+
+const HayPacientesDePediatria = (pacientes: Pacientes[]): boolean => {
+  let pacientesPediatria = false;
+
+  for (let i = 0; i < pacientes.length; i++) {
+    if (pacientes[i].especialidad === "Pediatra") {
+      pacientesPediatria = true;
+    }
+  }
+  return pacientesPediatria;
+};
+const pacientesParaElPediatra = HayPacientesDePediatria(pacientes);
+console.log(pacientesParaElPediatra);
+
+// Apartado Opcional
+//Queremos calcular el número total de pacientes que están asignados a la especialidad de Medico de familia, y lo que están asignados a Pediatría y a cardiología
+
+interface NumeroPacientesPorEspecialidad {
+  medicoDeFamilia: number;
+  pediatria: number;
+  cardiologia: number;
 }
 
-const btnNuevaPartida = document.getElementById("btn-nuevaPartida");
-if (
-  btnNuevaPartida !== undefined &&
-  btnNuevaPartida !== null &&
-  btnNuevaPartida instanceof HTMLButtonElement
-) {
-  btnNuevaPartida.addEventListener("click", nuevaPartida);
-}
-
-const btnQueHabriaPasado = document.getElementById("btn-queHabriaPasado");
-if (
-  btnQueHabriaPasado !== undefined &&
-  btnQueHabriaPasado !== null &&
-  btnQueHabriaPasado instanceof HTMLButtonElement
-) {
-  btnQueHabriaPasado.addEventListener("click", simularPartida);
-}
+const cuentaPacientesPorEspecialidad = (
+  pacientes: Pacientes[]
+): NumeroPacientesPorEspecialidad => {
+  const recuentoDePacientes: NumeroPacientesPorEspecialidad = {
+    medicoDeFamilia: 0,
+    pediatria: 0,
+    cardiologia: 0,
+  };
+  for (let i = 0; i < pacientes.length; i++) {
+    if (pacientes[i].especialidad === "Medico de familia") {
+      recuentoDePacientes.medicoDeFamilia++;
+    } else if (pacientes[i].especialidad === "Pediatra") {
+      recuentoDePacientes.pediatria++;
+    } else if (pacientes[i].especialidad === "Cardiólogo") {
+      recuentoDePacientes.cardiologia++;
+    }
+  }
+  return recuentoDePacientes;
+};
+const todosPacientes = cuentaPacientesPorEspecialidad(pacientes);
+console.log(todosPacientes);
